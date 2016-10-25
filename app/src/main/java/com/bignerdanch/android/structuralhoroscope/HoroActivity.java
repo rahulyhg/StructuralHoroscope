0c6ac4431200a6071fc624bf1bf9cf4c4145e0f7
Valocop <Andrei.Zdanovich@gmail.com>
2016-10-26 00:38:05 +0300

    add Picker Dialog

--- app/src/main/java/com/bignerdanch/android/structuralhoroscope/HoroActivity.java
+++ app/src/main/java/com/bignerdanch/android/structuralhoroscope/HoroActivity.java
@@ -1,15 +1,19 @@
 package com.bignerdanch.android.structuralhoroscope;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -18,12 +22,14 @@ import java.util.Map;
 public class HoroActivity extends AppCompatActivity {
 
     Button butAcept;
-    EditText birthDay;
+    TextView birthDay;
     TextView infoZodiak, znakZodiak;
     int[][] zodiakYears = new int[12][11];
-    Date dateOfBirthday;
-    int[] dateParseInt;
+
     String[] whatIsMean;
+
+    private final int DATE_DIALOG_ID = 0;
+    private int mDay, mMonth, mYear;
     String[] zodiakHoroscope = new String[]{"козерог", "водолей", "рыбы", "овен", "телец", "близнецы",
             "рак", "лев", "дева", "весы", "скорпион", "стрелец"};
 
@@ -45,18 +51,18 @@ public class HoroActivity extends AppCompatActivity {
             {3, 6, 1, 6, 3, 4, 5, 7, 4, 7, 5, 4} // свинья
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horo);
 
         butAcept = (Button) findViewById(R.id.butacept);
-        birthDay = (EditText) findViewById(R.id.birthdate);
+        birthDay = (TextView) findViewById(R.id.birthdate);
         infoZodiak = (TextView) findViewById(R.id.textinfozodiak);
         znakZodiak = (TextView) findViewById(R.id.textznakzodiak);
         whatIsMean = getResources().getStringArray(R.array.means);
 
+//add years to zodiakYears
         for (int i = 0; i < 12; i++) {
             int year = 1900 + i;
             for (int j = 0; j < 11; j++) {
@@ -66,54 +72,62 @@ public class HoroActivity extends AppCompatActivity {
             }
             System.out.println();
         }
+//add current date to TextViwe
+        final Calendar calendar = Calendar.getInstance();
+        mYear = calendar.get(Calendar.YEAR);
+        mMonth = calendar.get(Calendar.MONTH);
+        mDay = calendar.get(Calendar.DAY_OF_MONTH) + 1;
+        updateDisplay();
 
-//        add new function
+//add Date Picker Dialog
         butAcept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String birthday = birthDay.getText().toString();
-                boolean isCurrentDate = isCurrentDate(birthday);
-
-                if (isCurrentDate) {
-                    infoZodiak.setText("Дата рождения:\n" + dateOfBirthday);
-                    int chinaHoroPos = chinaHoro(dateParseInt[2]);
-                    int zodiakHoroPos = zodiakHoro(dateParseInt[0], dateParseInt[1]);
-                    infoZodiak.setText(infoZodiak.getText() + "\nЗнак зодиака: " +zodiakHoroscope[zodiakHoroPos] + "\nЗнак по китайскому гороскопу: " +
-                            zodiakChinaHoro[chinaHoroPos]);
-                    znakZodiak.setText(whatIsMean[zodiakMean[chinaHoroPos][zodiakHoroPos] - 1]);
-
-                } else {
-                    infoZodiak.setText("Введите верную дату");
-                    znakZodiak.setText("");
-                }
+                showDialog(DATE_DIALOG_ID);
             }
         });
 
     }
 
-    public boolean isCurrentDate(String date) {
-        if (date.matches("\\d{2}[.]{1}\\d{2}[.]{1}\\d{4}")) {
-            String[] dates = date.split("\\.");
-            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
-
-            try {
-                dateOfBirthday = dateFormat.parse(date);
-            } catch (ParseException e) {
-                System.out.println("Exception date");
-            }
-
-            int day = Integer.parseInt(dates[0]);
-            int months = Integer.parseInt(dates[1]);
-            int year = Integer.parseInt(dates[2]);
-            dateParseInt = new int[]{day, months, year};
-
-//            System.out.println(day + " " + months + " " + year);
-
-            if ((day > 0 && day <= 31) && (months > 0 && months <= 12) && (year >= 1900 && year <= 2031)) {
-                return true;
-            }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        mYear = year;
+                        mMonth = month;
+                        mDay = dayOfMonth;
+                        if (isCurrentDate(mYear)) {
+                            updateDisplay();
+                            showInfo(mYear, mMonth, mDay);
+                        }
+                        else inccorectDate();
+                    }
+                }, mYear, mMonth, mDay);
         }
+        return null;
+    }
+
+    public boolean isCurrentDate(int mYear) {
+        if (mYear <= 2031 && mYear >= 1900) return true;
+
         return false;
+    }
+
+    public void showInfo(int mYear, int mMonth, int mDay){
+        int IntchinaHoroscope = chinaHoro(mYear);
+        int IntzodiakHoroscope = zodiakHoro(mDay, mMonth);
+        infoZodiak.setText("Знак зодиака: " + zodiakHoroscope[IntzodiakHoroscope] + "\nЗнак по китайскому гороскопу: " +
+        zodiakChinaHoro[IntchinaHoroscope]);
+        znakZodiak.setText(whatIsMean[zodiakMean[IntchinaHoroscope][IntzodiakHoroscope] - 1]);
+    }
+
+    public void inccorectDate(){
+        birthDay.setText("Введите дату");
+        infoZodiak.setText("");
+        znakZodiak.setText("");
     }
 
     public int chinaHoro(int year) {
@@ -127,34 +141,29 @@ public class HoroActivity extends AppCompatActivity {
     }
 
     public int zodiakHoro(int day, int month) {
-        String StrDayMonth = month + "." + day;
+        String StrDayMonth = String.format("%d.%02d", month + 1, day);
+//                (month + 1) + "." + day;
         double DoubDayMonth = Double.parseDouble(StrDayMonth);
+        System.out.println(DoubDayMonth);
 
         if (DoubDayMonth >= 3.21 && DoubDayMonth <= 4.19) return 3;
-        else
-        if (DoubDayMonth >= 4.20 && DoubDayMonth <= 5.20) return 4;
-        else
-        if (DoubDayMonth >= 5.21 && DoubDayMonth <= 6.21) return 5;
-        else
-        if (DoubDayMonth >= 6.22 && DoubDayMonth <= 7.22) return 6;
-        else
-        if (DoubDayMonth >= 7.23 && DoubDayMonth <= 8.22) return 7;
-        else
-        if (DoubDayMonth >= 8.23 && DoubDayMonth <= 9.22) return 8;
-        else
-        if (DoubDayMonth >= 9.23 && DoubDayMonth <= 10.22) return 9;
-        else
-        if (DoubDayMonth >= 10.23 && DoubDayMonth <= 11.22) return 10;
-        else
-        if (DoubDayMonth >= 11.23 && DoubDayMonth <= 12.21) return 11;
-        else
-        if ((DoubDayMonth >= 12.22 && DoubDayMonth <= 12.31) ||
+        else if (DoubDayMonth >= 4.20 && DoubDayMonth <= 5.20) return 4;
+        else if (DoubDayMonth >= 5.21 && DoubDayMonth <= 6.21) return 5;
+        else if (DoubDayMonth >= 6.22 && DoubDayMonth <= 7.22) return 6;
+        else if (DoubDayMonth >= 7.23 && DoubDayMonth <= 8.22) return 7;
+        else if (DoubDayMonth >= 8.23 && DoubDayMonth <= 9.22) return 8;
+        else if (DoubDayMonth >= 9.23 && DoubDayMonth <= 10.22) return 9;
+        else if (DoubDayMonth >= 10.23 && DoubDayMonth <= 11.22) return 10;
+        else if (DoubDayMonth >= 11.23 && DoubDayMonth <= 12.21) return 11;
+        else if ((DoubDayMonth >= 12.22 && DoubDayMonth <= 12.31) ||
                 (DoubDayMonth >= 1.1 && DoubDayMonth <= 1.19)) return 0;
-        else
-        if (DoubDayMonth >= 1.20 && DoubDayMonth <= 2.18) return 1;
-        else
-        if (DoubDayMonth >= 2.19 && DoubDayMonth <= 3.20) return 2;
+        else if (DoubDayMonth >= 1.20 && DoubDayMonth <= 2.18) return 1;
+        else if (DoubDayMonth >= 2.19 && DoubDayMonth <= 3.20) return 2;
 
         return -1;
+    }
+
+    public void updateDisplay() {
+        birthDay.setText(String.format("%02d.%02d.%04d", mDay, mMonth + 1, mYear));
     }
 }
